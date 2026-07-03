2ec0ea460908732a07be77895a4048caf27a2b8e
fazlerabbi13 <mohammad0132794@gmail.com>
2026-07-03 14:42:15 +0600

    find indices for i < j > k.

---
+++ Educational/r91A.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    for (int i = 1; i < n - 1; i++)
+    {
+        if (a[i] > a[i - 1] && a[i] > a[i + 1])
+        {
+            cout << "YES" << "\n";
+            cout << i << " " << i + 1 << " " << i + 2 << "\n";
+            return;
+        }
+    }
+    cout << "NO" << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+}
